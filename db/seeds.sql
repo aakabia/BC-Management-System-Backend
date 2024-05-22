57e2f31e2ba249bc3f063898a994e789aaff1e75
Aziz Kabia <158849915+aakabia@users.noreply.github.com>
2024-05-22 15:47:23 -0400

    created test tabels

---
+++ db/seeds.sql
@@ -0,0 +1,26 @@
+INSERT INTO department (name)
+VALUES
+    ('Human Resources'),
+    ('Stow'),
+    ('Pick'),
+    ('AFM'),
+    ('Ship Dock');
+
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+    ('Associate', 47000, 3),
+    ('Area Manager', 62500, 2),
+    ('HR Manager', 77000, 1),
+    ('Operations Manger',93000, 4),
+    ('Senior Manager',110000, 5);
+    */
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+    ('Aziz', 'Kabia', 2, 3),
+    ('Kurtis', 'Backs', 5,NULL),
+    ('Maira','Ochoa', 4,2),
+    ('Tommy','Gonzales', 1,1),
+    ('Jabray','Cline', 3,NULL);
